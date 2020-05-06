b6f2d7afc9f111b937a3b62291a42d450a06afef
Akhil MS <akhilsree001@gmail.com>
2020-05-06 13:20:03 +0530

    Tulip:Havoc BringUp
    Signed-off-by: Akhil MS <akhilsree001@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tulip.mk
+    $(LOCAL_DIR)/havoc_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tulip-user \
-    lineage_tulip-userdebug \
-    lineage_tulip-eng
+    havoc_tulip-user \
+    havoc_tulip-userdebug \
+    havoc_tulip-eng
