f623bde78f1ed5f15f3eb784eb318609e4b4546c
Akhil MS <akhilsree001@gmail.com>
2020-09-15 11:39:39 +0530

    tulip: Update build FP to coral september

--- havoc_tulip.mk
+++ havoc_tulip.mk
@@ -38,7 +38,7 @@ TARGET_VENDOR_PRODUCT_NAME := tulip
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V11.0.3.0.PEKMIXM release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
 
 # Havoc
 export export HAVOC_BUILD_TYPE=Official
